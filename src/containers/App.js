8963985c4bbd63fbe9195a02a61f8f7adbb762b7
Aliel Reyes <alielreyes@gmail.com>
2019-10-12 18:28:26 -0700

    Adding files

---
+++ src/containers/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+/**
+ * Task Planner:
+ * Navbar
+ * 1. Logo
+ */
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Aliel, Cool we are ready to go now.</h1>
+    </div>
+  );
+}
+
+export default App;
