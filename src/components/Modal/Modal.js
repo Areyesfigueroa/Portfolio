0ca7861f0135d51ca8019061aa727407f3da5b2b
Aliel Reyes <alielreyes@gmail.com>
2019-10-28 21:12:09 -0700

    Modal Closing button finished.

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import classes from '../../containers/App.module.css';
 import { Spring } from 'react-spring/renderprops';
-import {Carousel} from 'react-bootstrap';
+import {Carousel, Button} from 'react-bootstrap';
 import InfoBox from '../InfoBox/InfoBox';
 
 //TODO: Create animation for fadein modal. 
@@ -31,6 +31,7 @@ const Modal = (props) => {
             {style => (
                 <div id={props.id} style={{...style, ...myStyle}} className={classes.modalShadow} onClick={props.clicked}>
                     <div className={classes.modal}>
+                        <a id={props.id} onClick={props.clicked} className={classes.closeBtn}>X</a>
                         <div className={classes.modalContent}>
                             <Carousel style={carouselStyle}>
                                 <Carousel.Item>
@@ -63,11 +64,16 @@ const Modal = (props) => {
                             </Carousel>
                             <InfoBox 
                             title= {props.card.title}
-                            subtitle= {props.card.tech}
+                            subtitle= {props.card.shortDesc}
                             >
                             {props.card.longDesc}
-                            </InfoBox>                            
+                            </InfoBox>
                         </div>
+                        <div className={classes.modalBtns}>
+                            <button className={classes.modalBtn}>Demo</button>
+                            <button className={classes.modalBtn}>Website</button>
+                            <button className={classes.modalBtn}>Github</button>
+                        </div>   
                     </div>
                 </div>
             )}
