febd9789f3e67ca31cc3a680db913a6f2582adc6
Aliel Reyes <alielreyes@gmail.com>
2020-02-06 08:06:39 -0800

    Creating first build

---
+++ build/precache-manifest.dedd3f5cf21a59c594b3e6bb6911b37c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c5804cc175419d02addeccbe4446b6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "485bd6f99f69a9f2b85a",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "055cc757c589685cff92",
+    "url": "/static/css/main.c436f9ce.chunk.css"
+  },
+  {
+    "revision": "485bd6f99f69a9f2b85a",
+    "url": "/static/js/2.fd84c00e.chunk.js"
+  },
+  {
+    "revision": "055cc757c589685cff92",
+    "url": "/static/js/main.2d4d0637.chunk.js"
+  },
+  {
+    "revision": "25293dcdd045d2663b65",
+    "url": "/static/js/runtime-main.67a84473.js"
+  },
+  {
+    "revision": "206f71b5aae1bbac79003dbec785ba9b",
+    "url": "/static/media/aliel-logo.206f71b5.png"
+  },
+  {
+    "revision": "cd092590209b6b5858170da7c023cb04",
+    "url": "/static/media/cardHeader.cd092590.png"
+  },
+  {
+    "revision": "8583cb4da0b47ffdcaff4bf38554ed46",
+    "url": "/static/media/header-bg.8583cb4d.png"
+  },
+  {
+    "revision": "d1e2cd9633a62c17ddf8f854ee7bf74c",
+    "url": "/static/media/home-modal.d1e2cd96.png"
+  },
+  {
+    "revision": "7a922e0ca0d888097cb38a4fd1beea02",
+    "url": "/static/media/home-page.7a922e0c.png"
+  },
+  {
+    "revision": "c1d2b04cf01c50469c24bb5ac97ffb1f",
+    "url": "/static/media/login-page.c1d2b04c.png"
+  },
+  {
+    "revision": "78895ad0b70045d83cf7438abea8621e",
+    "url": "/static/media/manage-users-actions.78895ad0.png"
+  },
+  {
+    "revision": "0fbd214d09660ad3d9ec6fdd81e0b5cc",
+    "url": "/static/media/manage-users.0fbd214d.png"
+  },
+  {
+    "revision": "126b1f7c465ad0bd76f3783180e3dd51",
+    "url": "/static/media/reset-pwd.126b1f7c.png"
+  },
+  {
+    "revision": "b49b72643e615ab810c589fbce7accf1",
+    "url": "/static/media/skateboard-example.b49b7264.png"
+  },
+  {
+    "revision": "003b8e87c9bfb9f5ba6f84e586b8ddf1",
+    "url": "/static/media/stock_img1.003b8e87.jpg"
+  },
+  {
+    "revision": "814dc171a2af578466cea54c6ce7d5a5",
+    "url": "/static/media/stock_img2.814dc171.jpg"
+  },
+  {
+    "revision": "d2c63e33f2f3d675c211d94190bc4677",
+    "url": "/static/media/stock_img3.d2c63e33.jpg"
+  }
+]);
