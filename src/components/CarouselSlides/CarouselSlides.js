28822aeac6d67cd73b14cdf6b1652f0c550701d8
Aliel Reyes <alielreyes@gmail.com>
2020-02-12 23:44:14 -0800

    Made changes to the carousel to take into account the image stretching. This will be the best it will get. Images still get crushed if the device is too small.

--- src/components/CarouselSlides/CarouselSlides.js
+++ src/components/CarouselSlides/CarouselSlides.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from '../../containers/App.module.css';
 import {Carousel} from 'react-bootstrap';
 
-const carouselImgStyles=['d-block', 'w-100', classes.carouselImg];
+const carouselImgStyles=['d-block', classes.carouselImg]; //w-100
 
 const carouselStyle = {
     width: "100%",
@@ -12,14 +12,16 @@ const carouselStyle = {
 
 const carouselSlides = (props) => {
     return (
-        <Carousel style={carouselStyle}>
+        <Carousel style={carouselStyle} interval={null} touch={true}>
             {props.slides.map((slide) => (
             <Carousel.Item key={slide.alt}>
-                <img 
-                className={carouselImgStyles.join(' ')}
-                src={slide.src}
-                alt={slide.alt}
-                />
+                <div className={classes.carouselImgContainer}>
+                    <img 
+                    className={carouselImgStyles.join(' ')}
+                    src={slide.src}
+                    alt={slide.alt}
+                    />
+                </div>
             </Carousel.Item>))}
         </Carousel>
         );
