947bf4b434a230ec8a1a212d56d2ca29f656fe81
Aliel Reyes <alielreyes@gmail.com>
2021-01-05 22:46:19 -0800

    Added new skills to the technology section

--- src/components/Sections/TechnologySection/TechnologySection.js
+++ src/components/Sections/TechnologySection/TechnologySection.js
@@ -18,7 +18,8 @@ const TechnologySection = () => {
                 { id: '0', value: 'HTML' },
                 { id: '1', value: 'CSS' },
                 { id: '2', value: 'Javascript' },
-                { id: '3', value: 'React' }
+                { id: '3', value: 'Bootstrap' },
+                { id: '4', value: 'React' }
             ]
         },
         {
@@ -26,9 +27,10 @@ const TechnologySection = () => {
             icon: gearsIcon,
             title: 'Server',
             items: [
-                { id: "0", value: 'PHP' },
-                { id: "1", value: 'Python' },
-                { id: "2", value: 'MySQL' },
+                { id: "0", value: 'Node.js' },
+                { id: "1", value: 'Express.js' },
+                { id: "2", value: 'PostgreSQL' },
+                { id: "3", value: 'MySQL' },
             ]
         },
         {
@@ -38,8 +40,9 @@ const TechnologySection = () => {
             items: [
                 { id: "0", value: 'NPM' },
                 { id: "1", value: 'Git/Github' },
-                { id: "2", value: 'SourceTree' },
-                { id: "3", value: 'Appsheet' }
+                { id: "2", value: 'Terminal' },
+                { id: "3", value: 'Heroku' },
+                { id: "4", value: 'Netlify' }
             ]
         }
     ];
